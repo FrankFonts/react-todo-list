24dc790804c4e2fd8104491770249577f9e677b9
Frank Fonts <9556806+FrankFonts@users.noreply.github.com>
2022-08-30 22:01:16 +0200

    How to bets update array state?

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React from "react";
 import "./styles.css";
 
-export default function ToDo() {
-  const [list, setList] = React.useState(["hi", "hello"]);
-  const handleAddTask = (event) => setList(list.push(event.target.value));
+export default function App() {
+  const [input, setInput] = React.useState([]);
+  const [todo, setTodo] = React.useState([]);
+
+  const handleInputOnchange = (event) => setInput(event.target.value);
+
+  const handleSubmit = () => {
+    todo.push({ id: input, title: input, checked: false });
+    setTodo(todo);
+    setInput("");
+    console.log(todo);
+  };
+
+  const List = () => {
+    return (
+      <>
+        <p>Your list</p>
+        <ul>
+          {todo.map((item) => {
+            return <li key={item.id}> {item.title}</li>;
+          })}
+        </ul>
+      </>
+    );
+  };
 
   return (
     <>
-      <input type="text" onSubmit={handleAddTask} />
-      <ul>
-        {list.map((item) => {
-          return <li>{item}</li>;
-        })}
-      </ul>
+      <input type="text" onChange={handleInputOnchange} value={input} />
+      <button type="submit" onClick={handleSubmit}>
+        Button
+      </button>
+      <div>{input}</div>
+      <List />
     </>
   );
 }
